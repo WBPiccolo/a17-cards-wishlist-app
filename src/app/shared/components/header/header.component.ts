8df2ed5a3c72df49491241d9772a3c66ee6b5b6c
Franceso Piccolo <piccolofrancesco39@gmail.com>
2024-03-22 20:23:57 +0100

    fix autocomplete display

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { MtgSet } from '../../../core/models/mtg-sets.model';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Observable, Subject, map, startWith, takeUntil } from 'rxjs';
+import { Observable, map, startWith } from 'rxjs';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -22,39 +22,33 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit, OnChanges {
+export class HeaderComponent implements OnChanges {
   @Input() sets: MtgSet[] = [];
   @Output() setSelected: EventEmitter<any> = new EventEmitter();
   @Output() selectDeselectClicked: EventEmitter<boolean> = new EventEmitter();
   @Output() getListClicked: EventEmitter<MouseEvent> = new EventEmitter();
 
-  //filteredSets: MtgSet[] = [];
-  setFormControl: FormControl = new FormControl<string>('');
-  destroy$: Subject<boolean> = new Subject();
-
-  myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
+  setFormControl = new FormControl<string | MtgSet>('');
   filteredSets: Observable<MtgSet[]> = new Observable();
 
-  ngOnInit() {
-    // this.filteredOptions = this.myControl.valueChanges.pipe(
-    //   startWith(''),
-    //   map(value => this._filter(value || '')),
-    // );
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes['sets'].currentValue) {
-      this.filteredSets = this.myControl.valueChanges.pipe(
+    if (changes['sets'].currentValue) {
+      this.filteredSets = this.setFormControl.valueChanges.pipe(
         startWith(''),
-        map(value => this._filter(value || '')),
+        map(value => {
+          const name = typeof value === 'string' ? value : value?.name;
+          return name ? this._filter(name as string) : this.sets.slice();
+        }),
       );
     }
   }
 
   private _filter(value: string): MtgSet[] {
     const filterValue = value.toLowerCase();
-    return this.sets.filter(set => set.name.toLowerCase().includes(filterValue));
+    return this.sets.filter(set => set.name.toLowerCase().includes(filterValue) || set.code.toLowerCase().includes(filterValue));
   }
-  
+
+  displayFn(user: MtgSet): string {
+    return user && user.name ? user.name : '';
+  }
 }
