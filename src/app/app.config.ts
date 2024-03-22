9b81999db0e73c4e07604a1dcde0dbc28c08e3e9
Franceso Piccolo <piccolofrancesco39@gmail.com>
2024-03-22 19:16:24 +0100

    added material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient()
+    provideHttpClient(), provideAnimationsAsync()
   ]
 };
